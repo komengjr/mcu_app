261f3f691c61e0d80a50ee0315215113af45938d
komengjr <47206605+komengjr@users.noreply.github.com>
2025-07-19 08:28:45 +0700

    iupda1231211

--- resources/views/kehadiran/form-sign.blade.php
+++ resources/views/kehadiran/form-sign.blade.php
@@ -49,6 +49,108 @@
     <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js"></script>
     <script type="text/javascript" src="http://keith-wood.name/js/jquery.signature.js"></script>
     <link rel="stylesheet" type="text/css" href="http://keith-wood.name/css/jquery.signature.css">
+    <script>
+        const mediaQuery = window.matchMedia('(max-width: 668px)');
+        var canvas = document.querySelector("canvas");
+        var ctx = canvas.getContext("2d");
+        if (mediaQuery.matches) {
+            var cw = (canvas.width = 300),
+                cx = cw / 2;
+            var ch = (canvas.height = 200),
+                cy = ch / 2;
+        } else {
+            var cw = (canvas.width = 450),
+                cx = cw / 2;
+            var ch = (canvas.height = 200),
+                cy = ch / 2;
+        }
+        ctx.strokeStyle = "#fff";
+        var dibujando = false;
+        var m = {
+            x: 0,
+            y: 0
+        };
+
+        var eventsRy = [{
+                event: "mousedown",
+                func: "onStart"
+            },
+            {
+                event: "touchstart",
+                func: "onStart"
+            },
+            {
+                event: "mousemove",
+                func: "onMove"
+            },
+            {
+                event: "touchmove",
+                func: "onMove"
+            },
+            {
+                event: "mouseup",
+                func: "onEnd"
+            },
+            {
+                event: "mouseout",
+                func: "onEnd"
+            },
+            {
+                event: "touchend",
+                func: "onEnd"
+            }
+        ];
+
+        function onStart(evt) {
+            m = oMousePos(canvas, evt);
+            ctx.beginPath();
+            dibujando = true;
+        }
+
+        function onMove(evt) {
+            if (dibujando) {
+                ctx.moveTo(m.x, m.y);
+                m = oMousePos(canvas, evt);
+                ctx.lineTo(m.x, m.y);
+                ctx.stroke();
+            }
+        }
+
+        function onEnd(evt) {
+            dibujando = false;
+            console.log(canvas);
+        }
+
+        function oMousePos(canvas, evt) {
+            var ClientRect = canvas.getBoundingClientRect();
+            var e = evt.touches ? evt.touches[0] : evt;
+            return {
+                x: Math.round(e.clientX - ClientRect.left),
+                y: Math.round(e.clientY - ClientRect.top)
+            };
+        }
+
+        for (var i = 0; i < eventsRy.length; i++) {
+            (function(i) {
+                var e = eventsRy[i].event;
+                var f = eventsRy[i].func;
+                console.log(f);
+                canvas.addEventListener(e, function(evt) {
+                    evt.preventDefault();
+                    window[f](evt);
+                    return;
+                }, false);
+            })(i);
+        }
+
+        clear.addEventListener(
+            "click",
+            function() {
+                ctx.clearRect(0, 0, cw, ch);
+            },
+            false
+        );
+    </script>
     {{-- <script>
         var isRTL = JSON.parse(localStorage.getItem('isRTL'));
         if (isRTL) {
@@ -216,107 +318,7 @@
             $("#signature64").val('');
         });
     </script>
-    <script>
-        const mediaQuery = window.matchMedia('(max-width: 668px)');
-        var canvas = document.querySelector("canvas");
-        var ctx = canvas.getContext("2d");
-        if (mediaQuery.matches) {
-            var cw = (canvas.width = 300),
-                cx = cw / 2;
-            var ch = (canvas.height = 200),
-                cy = ch / 2;
-        } else {
-            var cw = (canvas.width = 450),
-                cx = cw / 2;
-            var ch = (canvas.height = 200),
-                cy = ch / 2;
-        }
-        ctx.strokeStyle = "#fff";
-        var dibujando = false;
-        var m = {
-            x: 0,
-            y: 0
-        };
 
-        var eventsRy = [{
-                event: "mousedown",
-                func: "onStart"
-            },
-            {
-                event: "touchstart",
-                func: "onStart"
-            },
-            {
-                event: "mousemove",
-                func: "onMove"
-            },
-            {
-                event: "touchmove",
-                func: "onMove"
-            },
-            {
-                event: "mouseup",
-                func: "onEnd"
-            },
-            {
-                event: "mouseout",
-                func: "onEnd"
-            },
-            {
-                event: "touchend",
-                func: "onEnd"
-            }
-        ];
-
-        function onStart(evt) {
-            m = oMousePos(canvas, evt);
-            ctx.beginPath();
-            dibujando = true;
-        }
-
-        function onMove(evt) {
-            if (dibujando) {
-                ctx.moveTo(m.x, m.y);
-                m = oMousePos(canvas, evt);
-                ctx.lineTo(m.x, m.y);
-                ctx.stroke();
-            }
-        }
-        function onEnd(evt) {
-            dibujando = false;
-            console.log(canvas);
-        }
-
-        function oMousePos(canvas, evt) {
-            var ClientRect = canvas.getBoundingClientRect();
-            var e = evt.touches ? evt.touches[0] : evt;
-            return {
-                x: Math.round(e.clientX - ClientRect.left),
-                y: Math.round(e.clientY - ClientRect.top)
-            };
-        }
-
-        for (var i = 0; i < eventsRy.length; i++) {
-            (function(i) {
-                var e = eventsRy[i].event;
-                var f = eventsRy[i].func;
-                console.log(f);
-                canvas.addEventListener(e, function(evt) {
-                    evt.preventDefault();
-                    window[f](evt);
-                    return;
-                }, false);
-            })(i);
-        }
-
-        clear.addEventListener(
-            "click",
-            function() {
-                ctx.clearRect(0, 0, cw, ch);
-            },
-            false
-        );
-    </script>
 </body>
 
 </html>

2cc5ba74358ccd601b18a5f17f985edb37f343d3
komengjr <47206605+komengjr@users.noreply.github.com>
2025-07-28 22:29:58 +0700

    updateselalu2

--- resources/views/application/menu/pengiriman/option-peserta.blade.php
+++ resources/views/application/menu/pengiriman/option-peserta.blade.php
@@ -1,7 +1,7 @@
- <div class="col" style="z-index: 996;">
+ <div class="col" style="z-index: 999;">
      <!-- <label for="organizerSingle" class="my-0">Pilih Peserta</label> -->
      <label for="organizerMultiple">Pilih Peserta</label>
-     <select class="form-select js-choice-peserta" id="pesertamcu" multiple="multiple" name="organizerMultiple" data-options='{"removeItemButton":true,"placeholder":true}'>
+     <select class="form-select js-choice-peserta" id="pesertamcu" multiple="multiple" name="organizerMultiple" data-options='{"removeItemButton":true,"placeholder":true}' style="z-index: 999;">
          <option value="">Cari Peserta...</option>
          @foreach ($data as $datas)
          <option value="{{$datas->mou_peserta_code }}">{{ $datas->mou_peserta_name }} - {{ $datas->mou_peserta_email }} - {{ $datas->mou_peserta_no_hp }}</option>

6beca9ba88314e56f867ecf20e95ead32614d69e
komengjr <47206605+komengjr@users.noreply.github.com>
2025-08-29 08:03:30 +0700

    updateselalu111

--- resources/views/application/menu/mcu/form-report-absensi-mcu.blade.php
+++ resources/views/application/menu/mcu/form-report-absensi-mcu.blade.php
@@ -1,6 +1,6 @@
 <div class="modal-body p-0">
     <div class="bg-300 rounded-top-lg py-3 ps-4 pe-6">
-        <h4 class="mb-1" id="staticBackdropLabel">Daftar Kehadiran MCU</h4>
+        <h4 class="mb-1" id="staticBackdropLabel">Daftar Kehadiran MCU : Total Kehadiran Peserta <strong>{{ $peserta }}</strong></h4>
         <p class="fs--2 mb-0">Support by <a class="link-600 fw-semi-bold" href="#!">Transforma</a></p>
     </div>
     <div class="p-2 pb-0">

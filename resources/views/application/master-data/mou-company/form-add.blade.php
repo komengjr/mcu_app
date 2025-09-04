6b8fdf0bd6879947578c36241bec5ebea841ac84
komengjr <47206605+komengjr@users.noreply.github.com>
2025-09-04 10:13:52 +0700

    updaegit11

--- resources/views/application/master-data/mou-company/form-add.blade.php
+++ resources/views/application/master-data/mou-company/form-add.blade.php
@@ -1,6 +1,6 @@
 <div class="modal-body p-0">
     <div class="bg-light rounded-top-lg py-3 ps-4 pe-6">
-        <h4 class="mb-1" id="staticBackdropLabel">Add Perusahaan</h4>
+        <h4 class="mb-1" id="staticBackdropLabel">Add MOU Perusahaan</h4>
         <p class="fs--2 mb-0">Support by <a class="link-600 fw-semi-bold" href="#!">Transforma</a></p>
     </div>
     <form class="row g-3 p-4" action="{{ route('mou_company_save') }}" method="post" enctype="multipart/form-data">

5b3145febe72daff3c3a560f8203940fd4fce0db
komengjr <47206605+komengjr@users.noreply.github.com>
2025-09-18 07:07:31 +0700

    updateselalu

--- resources/views/application/dashboard/monitoring/data-peserta-full-rekap.blade.php
+++ resources/views/application/dashboard/monitoring/data-peserta-full-rekap.blade.php
@@ -2,36 +2,46 @@
     <div class="bg-danger rounded-top-lg py-3 ps-4 pe-6">
         <h4 class="mb-1 text-white" id="staticBackdropLabel">Data Peserta MCU<strong>{{ $data->master_company_name }} -
                 {{ $data->company_mou_name }}</strong></h4>
-                <input type="text" name="code_perusahaan" id="code_perusahaan" value="{{ $data->master_company_code }}" hidden>
-                <input type="text" name="code_mou" id="code_mou" value="{{ $data->company_mou_code }}" hidden>
+        <input type="text" name="code_perusahaan" id="code_perusahaan" value="{{ $data->master_company_code }}" hidden>
+        <input type="text" name="code_mou" id="code_mou" value="{{ $data->company_mou_code }}" hidden>
         <p class="text-white fs--2 mb-0">Support by <a class="fw-semi-bold text-white" href="#!">Transforma</a></p>
     </div>
     <div class="tab-content p-3">
         <div class="card mb-3 border">
             <div class="card-body d-flex justify-content-between">
                 <div>
-                    <a class="btn btn-falcon-default" href="#" data-bs-toggle="tooltip" data-bs-placement="top" title="" data-bs-original-title="Back to inbox" aria-label="Back to inbox">
+                    <a class="btn btn-falcon-default" href="#" data-bs-toggle="tooltip" data-bs-placement="top" title=""
+                        data-bs-original-title="Back to inbox" aria-label="Back to inbox">
                         <span class="fas fa-network-wired"></span>
                     </a>
                     <span class="mx-1 mx-sm-2 text-300">|</span>
                     @foreach ($paket as $pak)
-                    <button class="btn btn-falcon-default btn-sm mx-1" type="button" id="button-monitoring-pilih-paket" data-code="{{$pak->company_mou_code}}" data-id="{{$pak->mou_agreement_code}}">
-                        {{$pak->mou_agreement_name}}
-                    </button>
+                        <button class="btn btn-falcon-default btn-sm mx-1" type="button" id="button-monitoring-pilih-paket"
+                            data-code="{{$pak->company_mou_code}}" data-id="{{$pak->mou_agreement_code}}">
+                            {{$pak->mou_agreement_name}}
+                        </button>
                     @endforeach
                 </div>
                 <div class="d-flex">
                     <div class="d-none d-md-block">
-                        <select name="lokasi_perusahaan" class="form-control choices-single-lokasi" id="lokasi_perusahaan">
+                        <select name="lokasi_perusahaan" class="form-control choices-single-lokasi"
+                            id="lokasi_perusahaan">
                             <option value="">Pilih Lokasi Perusahaan</option>
                             @foreach ($lokasi as $lok)
-                            <option value="{{ $lok->company_location_code  }}">{{ $lok->company_location_name }}</option>
+                                <option value="{{ $lok->company_location_code  }}">{{ $lok->company_location_name }}
+                                </option>
                             @endforeach
                         </select>
                     </div>
                     <div class="dropdown font-sans-serif">
-                        <button class="btn btn-falcon-default text-600 btn-sm dropdown-toggle dropdown-caret-none ms-2" type="button" id="email-settings" data-bs-toggle="dropdown" data-boundary="viewport" aria-haspopup="true" aria-expanded="false"><svg class="svg-inline--fa fa-cog fa-w-16" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="cog" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" data-fa-i2svg="">
-                                <path fill="currentColor" d="M487.4 315.7l-42.6-24.6c4.3-23.2 4.3-47 0-70.2l42.6-24.6c4.9-2.8 7.1-8.6 5.5-14-11.1-35.6-30-67.8-54.7-94.6-3.8-4.1-10-5.1-14.8-2.3L380.8 110c-17.9-15.4-38.5-27.3-60.8-35.1V25.8c0-5.6-3.9-10.5-9.4-11.7-36.7-8.2-74.3-7.8-109.2 0-5.5 1.2-9.4 6.1-9.4 11.7V75c-22.2 7.9-42.8 19.8-60.8 35.1L88.7 85.5c-4.9-2.8-11-1.9-14.8 2.3-24.7 26.7-43.6 58.9-54.7 94.6-1.7 5.4.6 11.2 5.5 14L67.3 221c-4.3 23.2-4.3 47 0 70.2l-42.6 24.6c-4.9 2.8-7.1 8.6-5.5 14 11.1 35.6 30 67.8 54.7 94.6 3.8 4.1 10 5.1 14.8 2.3l42.6-24.6c17.9 15.4 38.5 27.3 60.8 35.1v49.2c0 5.6 3.9 10.5 9.4 11.7 36.7 8.2 74.3 7.8 109.2 0 5.5-1.2 9.4-6.1 9.4-11.7v-49.2c22.2-7.9 42.8-19.8 60.8-35.1l42.6 24.6c4.9 2.8 11 1.9 14.8-2.3 24.7-26.7 43.6-58.9 54.7-94.6 1.5-5.5-.7-11.3-5.6-14.1zM256 336c-44.1 0-80-35.9-80-80s35.9-80 80-80 80 35.9 80 80-35.9 80-80 80z"></path>
+                        <button class="btn btn-falcon-default text-600 btn-sm dropdown-toggle dropdown-caret-none ms-2"
+                            type="button" id="email-settings" data-bs-toggle="dropdown" data-boundary="viewport"
+                            aria-haspopup="true" aria-expanded="false"><svg class="svg-inline--fa fa-cog fa-w-16"
+                                aria-hidden="true" focusable="false" data-prefix="fas" data-icon="cog" role="img"
+                                xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" data-fa-i2svg="">
+                                <path fill="currentColor"
+                                    d="M487.4 315.7l-42.6-24.6c4.3-23.2 4.3-47 0-70.2l42.6-24.6c4.9-2.8 7.1-8.6 5.5-14-11.1-35.6-30-67.8-54.7-94.6-3.8-4.1-10-5.1-14.8-2.3L380.8 110c-17.9-15.4-38.5-27.3-60.8-35.1V25.8c0-5.6-3.9-10.5-9.4-11.7-36.7-8.2-74.3-7.8-109.2 0-5.5 1.2-9.4 6.1-9.4 11.7V75c-22.2 7.9-42.8 19.8-60.8 35.1L88.7 85.5c-4.9-2.8-11-1.9-14.8 2.3-24.7 26.7-43.6 58.9-54.7 94.6-1.7 5.4.6 11.2 5.5 14L67.3 221c-4.3 23.2-4.3 47 0 70.2l-42.6 24.6c-4.9 2.8-7.1 8.6-5.5 14 11.1 35.6 30 67.8 54.7 94.6 3.8 4.1 10 5.1 14.8 2.3l42.6-24.6c17.9 15.4 38.5 27.3 60.8 35.1v49.2c0 5.6 3.9 10.5 9.4 11.7 36.7 8.2 74.3 7.8 109.2 0 5.5-1.2 9.4-6.1 9.4-11.7v-49.2c22.2-7.9 42.8-19.8 60.8-35.1l42.6 24.6c4.9 2.8 11 1.9 14.8-2.3 24.7-26.7 43.6-58.9 54.7-94.6 1.5-5.5-.7-11.3-5.6-14.1zM256 336c-44.1 0-80-35.9-80-80s35.9-80 80-80 80 35.9 80 80-35.9 80-80 80z">
+                                </path>
                             </svg><!-- <span class="fas fa-cog"></span> Font Awesome fontawesome.com --></button>
                         <div class="dropdown-menu dropdown-menu-end border py-2" aria-labelledby="email-settings">
                             <!-- <a class="dropdown-item" href="#!">Configure inbox</a>
@@ -43,57 +53,12 @@
             </div>
         </div>
         <span id="peserta-monitoring-mcu">
-            <table id="example" class="table table-striped" style="width:100%">
-                <thead class="bg-200 text-700 fs--2">
-                    <tr>
-                        <th>NO</th>
-                        <th>NIP</th>
-                        <th>NAMA PESERTA</th>
-                        <th>JENIS KELAMIN</th>
-                        <th>DEPARTEMEN</th>
-                        <th>WILAYAH</th>
-                        <th>LOKASI MCU </th>
-                        @foreach ($pem as $pems)
-                        <th>{{$pems->master_pemeriksaan_name}}</th>
-                        @endforeach
-
-                    </tr>
-                </thead>
-                <tbody class="fs--2">
-                    @php
-                    $no = 1;
-                    @endphp
-                    @foreach ($peserta as $pes)
-                    <tr>
-                        <td>{{ $no++ }}</td>
-                        <td>{{ $pes->mou_peserta_nip }}</td>
-                        <td>{{ $pes->mou_peserta_name }}</td>
-                        <td>{{ $pes->mou_peserta_jk }}</td>
-                        <td>{{ $pes->mou_peserta_departemen }}</td>
-                        <td>{{ $pes->group_cabang_name }}</td>
-                        <td>{{ $pes->master_cabang_name }}</td>
-                        @foreach ($pem as $pems)
-                        <?php
-                        $status = DB::table('log_pemeriksaan_pasien')
-                            ->where('mou_peserta_code', $pes->mou_peserta_code)
-                            ->where('master_pemeriksaan_code', $pems->master_pemeriksaan_code)->first();
-                        ?>
-                        <th class="text-center fs--1">
-                            @if ($status)
-                            @if ($status->log_pemeriksaan_status == 1)
-                            <span style="color: green;" type="button" data-bs-toggle="tooltip" data-bs-placement="right" title="Sudah Diperiksa">Y</span>
-                            @else
-                            <span class="text-warning" type="button" data-bs-toggle="tooltip" data-bs-placement="right" title="{{$status->log_pemeriksaan_deskripsi}}">!</span><span> : {{$status->log_pemeriksaan_deskripsi}}</span>
-                            @endif
-                            @else
-                            <span class="text-danger" type="button" data-bs-toggle="tooltip" data-bs-placement="right" title="Belum Diperiksa">X</span>
-                            @endif
-                        </th>
-                        @endforeach
-                    </tr>
-                    @endforeach
-                </tbody>
-            </table>
+            <div class="alert alert-success" role="alert">
+                <h4 class="alert-heading fw-semi-bold">Well done!</h4>
+                <p>Silahkan Untuk Memilih Paket terlebih dahulu.</p>
+                <hr />
+                <p class="mb-0">Terima Kasih.</p>
+            </div>
         </span>
     </div>
 </div>
@@ -119,7 +84,7 @@
     new window.Choices(document.querySelector(".choices-single-lokasi"));
 </script>
 <script>
-    $('#lokasi_perusahaan').on("change", function() {
+    $('#lokasi_perusahaan').on("change", function () {
         var dataid = document.getElementById("lokasi_perusahaan").value;
 
         var code_mou = document.getElementById("code_mou").value;
@@ -143,9 +108,9 @@
                     "code_mou": code_mou,
                 },
                 dataType: 'html',
-            }).done(function(data) {
+            }).done(function (data) {
                 $("#peserta-monitoring-mcu").html(data);
-            }).fail(function() {
+            }).fail(function () {
                 $("#peserta-monitoring-mcu").html('error');
             });
         }

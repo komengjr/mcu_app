80fb3e2f1d0b298fcecd6bf459294b3d87e67f0f
komengjr <47206605+komengjr@users.noreply.github.com>
2026-09-21 15:51:55 +0700

    updatatabaru1

--- resources/views/application/laporan/report/pdf-perorangan-pemeriksaan.blade.php
+++ resources/views/application/laporan/report/pdf-perorangan-pemeriksaan.blade.php
@@ -262,7 +262,7 @@ return ['label' => 'Prehipertensi', 'color' => '#b7791f', 'bg' => '#fefcbf'];
                                 <td>{{ $detail->mou_peserta_departemen ?? '-' }}</td>
                                 <td class="label">Tanggal Input</td>
                                 <td>:</td>
-                                <td>{{ $detail->created_at ?? '-' }}</td>
+                                <td>{{ $detail->created_at ?? '' }}</td>
                             </tr>
                         </table>
 
